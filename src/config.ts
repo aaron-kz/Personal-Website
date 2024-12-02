3392670df058d7b3d33985bfa7a75da9ecedd661
aaron-kz <im.aaronkz@gmail.com>
2024-12-01 23:02:27 -0500

    Fix: Site description

--- src/config.ts
+++ src/config.ts
@@ -3,6 +3,6 @@
 
 export const SITE_TITLE = "Aaron KZ | Web Developer";
 export const SITE_DESCRIPTION =
-  "Hago tus ideas digitales realid. Creo landing pages, catálogos online, blogs y más.";
+  "Creación de soluciones web personalizadas.";
 export const GENERATE_SLUG_FROM_TITLE = true;
 export const TRANSITION_API = true;
