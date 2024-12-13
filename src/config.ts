dc83ba1332d00c6f2735421f136e955d8158b2cd
aaron-kz <im.aaronkz@gmail.com>
2024-12-13 16:44:54 -0500

    -

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Aaron KZ | Web Developer";
-export const SITE_DESCRIPTION =
-  "Creación de soluciones web personalizadas.";
+export const SITE_DESCRIPTION = "Creación de soluciones web personalizadas.";
 export const GENERATE_SLUG_FROM_TITLE = true;
 export const TRANSITION_API = true;
