8f7ecee49dda6f952a7e32ea727972d997af6613
aaron-kz <im.aaronkz@gmail.com>
2024-12-23 01:30:34 -0500

    Add keywords

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,8 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Aaron KZ | Web Developer";
-export const SITE_DESCRIPTION = "Creación de soluciones web personalizadas.";
+export const SITE_DESCRIPTION =
+  "Aaron KZ. Creación de soluciones web personalizadas.";
 export const GENERATE_SLUG_FROM_TITLE = true;
+export const SITE_KEYWORDS = "Aaron KZ, web developer, desarrollo web";
 export const TRANSITION_API = true;
